6f196fe79330e64a79ef664718ea3ab038583703
Otavio <otamm@hotmail.com>
2014-12-12 13:41:43 -0200

    Initial Commit

---
+++ Shoe_Size_Converter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Shoe_Size_Converter
+//
+//  Created by Otavio Monteagudo on 12/12/14.
+//  Copyright (c) 2014 Otavio Monteagudo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Shoe_Size_ConverterTests/Shoe_Size_ConverterTests.swift
@@ -0,0 +1,36 @@
+//
+//  Shoe_Size_ConverterTests.swift
+//  Shoe_Size_ConverterTests
+//
+//  Created by Otavio Monteagudo on 12/12/14.
+//  Copyright (c) 2014 Otavio Monteagudo. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Shoe_Size_ConverterTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
