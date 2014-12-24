497c0683ccea1bbe1fff053d7d69fabeba811474
Otavio <otamm@hotmail.com>
2014-12-24 04:30:17 -0200

    Debugged connections, removed control flow.

--- Shoe_Size_Converter/ViewController.swift
+++ Shoe_Size_Converter/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class FirstViewController: UIViewController {
 
     @IBOutlet weak var inputSizeMen: UITextField!
     
@@ -31,38 +31,37 @@ class ViewController: UIViewController {
 
     //custom functions
     
-    @IBAction func convertMenShoeSizePressed(sender: UIButton) {
-            let sizeTextField = inputSizeMen.text.toInt()! + 30
-            //this captures the "text" value of the object inside a constant. It converts the value to Integer (the "!" is used to store "nil" in case the value cannot be converted toInt()) and adds the constant converter (+ 30) to change it to an European shoe size.
-            if (sizeTextField == Int()) {
-                menConvertedSize.text = "\(sizeTextField)" + " in European shoe size."
-                menConvertedSize.hidden = false
-            } else {
-                var alert_button = UIAlertView()
-                alert_button.title = "..."
-                alert_button.message = "I said WHOLE number"
-                alert_button.addButtonWithTitle("Bazomgna xD")
-                alert_button.show()
-            }
-        }
-    
-    @IBAction func convertedWomenShoeSizePressed(sender: UIButton) {
-        let sizeTextFieldW = Double((inputSizeWomen.text as NSString).doubleValue) //the way to convert a string using some Obj-C techniques and THEN converting it to a proper Swift Double.
-        // same as "sizeTextField",but converting to a Double/Float.
-        let conversionConstant = 30.5
-        // documentation for NSString:
-        //  "The NSString class declares the programmatic interface for an object that manages immutable strings. An immutable string is a text string that is defined when it is created and subsequently cannot be changed. NSString is implemented to represent an array of Unicode characters, in other words, a text string."
-        
-        if (sizeTextFieldW == Double()) {
-            womenConvertedSize.text = "\(sizeTextFieldW + conversionConstant)" + " in European shoe size."
-            womenConvertedSize.hidden = false
-        } else {
+    @IBAction func convertedMenShoeSizePressed(sender: UIButton) {
+        let sizeTextField = inputSizeMen.text.toInt()! + 30
+        //this captures the "text" value of the object inside a constant. It converts the value to Integer (the "!" is used to store "nil" in case the value cannot be converted toInt()) and adds the constant converter (+ 30) to change it to an European shoe size.
+   //     if (sizeTextField == Int()) {
+            menConvertedSize.text = "\(sizeTextField)" + " in European shoe size."
+            menConvertedSize.hidden = false
+    /*    } else {
             var alert_button = UIAlertView()
             alert_button.title = "..."
             alert_button.message = "I said WHOLE number"
             alert_button.addButtonWithTitle("Bazomgna xD")
             alert_button.show()
-        }
+        } */
+    }
+    
+    @IBAction func convertedWomenShoeSizePressed(sender: UIButton) {
+        let sizeTextField = Double((inputSizeWomen.text as NSString).doubleValue) //the way to convert a string using some Obj-C techniques and THEN converting it to a proper Swift Double.
+        // same as "sizeTextField",but converting to a Double/Float.
+        let conversionConstant = 30.5
+        // documentation for NSString:
+        //  "The NSString class declares the programmatic interface for an object that manages immutable strings. An immutable string is a text string that is defined when it is created and subsequently cannot be changed. NSString is implemented to represent an array of Unicode characters, in other words, a text string."
+        
+       // if (sizeTextField == Double()) {
+            womenConvertedSize.text = "\(sizeTextField + conversionConstant)" + " in European shoe size."
+            womenConvertedSize.hidden = false
+  /*      } else {
+            var alert_button = UIAlertView()
+            alert_button.title = "..."
+            alert_button.message = "I said WHOLE number"
+            alert_button.addButtonWithTitle("Bazomgna xD")
+            alert_button.show()
+        } */
     }
 }
-
